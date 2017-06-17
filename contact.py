d4344c163f44f6dad772a0c8cbc831055961122b
VampireSH <sherlockya@gmail.com>
2017-06-17 12:41:16 +0800

    input have color now

--- contact.py
+++ contact.py
@@ -17,6 +17,12 @@ contant = "\n\033[0;35mname\033[0m: \033[0;33m{}\033[0m\
            \n\033[0;35mphone number\033[0m: \033[0;33m{}\033[0m\
            \n\033[0;35mother\033[0m: \033[0;33m{}\033[0m\n"
 
+def _input(_contant):
+    print("{}{}{}".format(puple, _contant, white), end='')
+    result = input()
+    print("".format(backgroud))
+    return result
+
 
 class Contact:
 
@@ -26,9 +32,9 @@ class Contact:
         self.c = self.conn.cursor()
 
     def add(self):
-        name = input('{}Please input name:{} '.format(*title_color))
-        phone_num = input('{}Please input phone_num:{} '.format(*title_color))
-        other = input('{}Other messages?(optional){} '.format(*title_color))
+        name = _input('Please input name: ')
+        phone_num = _input('Please input phone_num: ')
+        other = _input('Other messages?(optional) ')
 
         if name == '' or phone_num == '':
             print('\n\n{}At least you should tell me require info{}'.format(*info_color))
@@ -44,7 +50,7 @@ class Contact:
             print('{}contact added{}'.format(*info_color))
 
     def search(self):
-        query = input('{}What do you want to search?{} '.format(*title_color))
+        query = _input('What do you want to search? ')
         print('\n\n')
         query = ('%{}%'.format(query), '%{}%'.format(query), '%{}%'.format(query))
 
@@ -67,7 +73,7 @@ class Contact:
             print("{}There's nothing here{}".format(*info_color))
 
     def delete(self):
-        query = input('{}Who do you want to forget?{} '.format(*title_color))
+        query = _input('Who do you want to forget? ')
 
         self.c.execute("SELECT * FROM CONTACT WHERE NAME = ? COLLATE NOCASE", (query,))
         result = self.c.fetchone()
@@ -77,7 +83,7 @@ class Contact:
         else:
             print('{}Sorry~ No result{}'.format(*info_color))
 
-        ask = input("{}Do you want to forget him/her? tell me (Y)es or (N)o:{} ".format(*title_color))
+        ask = _input("Do you want to forget him/her? tell me (Y)es or (N)o: ")
         print('\n\n')
 
         if ask.lower() in ('yes', 'y'):
@@ -89,17 +95,17 @@ class Contact:
             print("{}You still can't forget him/her, right?{}".format(*info_color))
 
     def update(self):
-        name = input('{}You have new info about who?{} '.format(title_color))
+        name = _input('{}You have new info about who?{} '.format(title_color))
         self.c.execute("SELECT * FROM CONTACT WHERE NAME = ?", (name,))
         result = self.c.fetchone()
         if type(result) == tuple:
             print(contant.format(*result))
 
-            new_contant = input(
+            new_contant = _input(
                 '{}What do you want to update, name, phone or other?{} '.format(title_color))
 
             if new_contant == 'name':
-                new_name = input('{}tell me new name:{} '.format(title_color))
+                new_name = _input('tell me new name: ')
                 if new_name:
                     self.c.execute("UPDATE CONTACT SET NAME = ? WHERE NAME = ?",
                                    (new_name, name))
@@ -108,7 +114,7 @@ class Contact:
                     print("{}You haven't change anything{}".format(*info_color))
 
             elif new_contant == 'phone':
-                new_ph_num = input('{}tell me new phone number:{} '.format(title_color))
+                new_ph_num = _input('tell me new phone number: ')
                 if new_ph_num:
                     self.c.execute("UPDATE CONTACT SET PHONE_NUM = ? WHERE NAME = ?",
                                    (new_ph_num, name))
@@ -117,7 +123,7 @@ class Contact:
                     print("{}You haven't change anything{}".format(*info_color))
 
             elif new_contant == 'other':
-                new_other = input('{}tell me new other messages:{} '.format(title_color))
+                new_other = _input('tell me new other messages: ')
                 if new_other:
                     self.c.execute("UPDATE CONTACT SET OTHER = ? WHERE NAME = ?",
                                    (new_other, name))
@@ -153,7 +159,7 @@ class Contact:
                    \ne. exit\n{}".format(*title_color)
 
             print(start)
-            choice = input('{}Your choice:{} '.format(*info_color))
+            choice = _input('Your choice: ')
             if choice == '1':
                 self.add()
 
